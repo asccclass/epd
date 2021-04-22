71660bfb111467d2dbbe1fcb801c1918de2c4357
Chih-Han Liu <andyliu@sinica.edu.tw>
2021-04-22 09:46:10 +0800

    <backup>

---
+++ bt.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+hciconfig
+HWID='01 4a 2b 4e 69'
+ADVERTISE="13 02 01 06 03 03 6F FE 0B 16 6F FE 02 ${HWID} 7F 00"
+sudo hciconfig hci0 up
+sudo hcitool -i hci0 cmd 0x08 0x008 ${ADVERTISE}
+sudo hciconfig hci0 leadv 3
