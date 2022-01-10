f131ad78561c5609076c828dc3239bcbeb7e38cd
C.H Liu－Jii <andyliu@sinica.edu.tw>
2022-01-10 11:35:18 +0800

    setup file

---
+++ initial.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+sudo echo "static domain_name_servers=8.8.8.8 140.109.1.10 1.1.1.1" >>  /etc/dhcpcd.conf
+
+sudo timedatectl set-timezone Asia/Taipei
+sudo apt-get install p7zip-full -y
+sudo ssh-keygen
+sudo raspi-config
+
+# BCM2835
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
+tar zxvf bcm2835-1.60.tar.gz
+cd bcm2835-1.60/
+sudo ./configure
+sudo make
+sudo make check
+sudo make install
+cd ../
+rm -rf bcm2835-1.60
+rm -rf bcm2835-1.60.tar.gz
+
+# wiringPi
+sudo apt-get install wiringpi -y
+cd /tmp
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+gpio -v
+rm -f wiringpi-latest.deb
+
+wget https://github.com/asccclass/epd/releases/download/v0.0.1/epd-master.zip
+unzip epd-master.zip
+mv epd-master epd
+mv epd /home/pi/
+chown pi /home/pi/epd
+chgrp pi /home/pi/epd
+chown pi /home/pi/epd/*
+chgrp pi /home/pi/epd/pic/*
+chown pi /home/pi/epd/pic/*
+chgrp pi /home/pi/epd/*
+
