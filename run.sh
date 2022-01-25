3bc03cfd32a63368ccafc7ab524ab3257e96db98
C.H Liu <justgps@gmail.com>
2022-01-25 11:25:43 +0800

    <feature>add close and bright led

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo none | sudo tee /sys/class/leds/led0/trigger
 rm -f /home/pi/epd/ashq-meeting-psk.json*
 cd /home/pi/epd/ && wget https://t02.w.ascc.net/wlan-ashq-meeting/ashq-meeting-psk.json
 cd /home/pi/epd/ && ./app
+
+echo 1 | sudo tee /sys/class/leds/led0/brightness
 
 ## for test: sudo /home/pi/epd/epd ASHQ_Meeting 43773477
